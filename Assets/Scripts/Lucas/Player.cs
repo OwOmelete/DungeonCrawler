546078f55e10282b180911a0144b2ff400a95869
Lucas <lucaskominiarz@gmail.com>
2025-02-05 00:45:36 +0100

    Personnages ajouts
    - personnage qui s'arrête lors de sa rotation
    - sprite qui s'inverse en fonction de si orienté a gauche ou a droite

--- Assets/Scripts/Lucas/Player.cs
+++ Assets/Scripts/Lucas/Player.cs
@@ -1,17 +1,38 @@
 using System;
 using Unity.VisualScripting;
 using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
 
     private float moveHorizontal = 0f;    
     private float moveVertical = 0f;
     [SerializeField] private float moveSpeed = 55f;
-    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Rotation rotationReference;
+    private Rigidbody2D rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
-        moveHorizontal = Input.GetAxisRaw("Horizontal");     
-        moveVertical = Input.GetAxisRaw("Vertical");      
-        rb.linearVelocity = new Vector2(moveHorizontal, moveVertical).normalized * moveSpeed;
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
+
+        if (rotationReference.canMove)
+        {
+            if (moveHorizontal != 0 || moveVertical != 0)
+            {
+                rb.linearVelocity = new Vector2(moveHorizontal, moveVertical).normalized * moveSpeed;
+            }
+            else
+            {
+                rb.linearVelocity = Vector2.zero;
+            }
+        }
+        else
+        {
+            rb.linearVelocity = Vector2.zero;
+        }
     }
 }
