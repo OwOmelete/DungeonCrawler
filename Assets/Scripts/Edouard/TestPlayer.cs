80962a43d4995a82eac7ca96e947dfadc36f8e00
E Laffont <laffont.edu@gmail.com>
2025-03-12 17:35:50 +0100

    Courrent finit et GD-Friendly

--- Assets/Scripts/Edouard/TestPlayer.cs
+++ Assets/Scripts/Edouard/TestPlayer.cs
@@ -4,21 +4,60 @@ using System.Collections;
 public class TestPlayer : MonoBehaviour
 {
     // ATTENTION!!! Ce script n'est qu'un test, ne pas le confondre avec le vrai PLAYER!
-    public int health;
     private int maxHealth = 100;
-
-    public int oxygen;
+    public int health;
+    
     private int maxOxygen = 100;
+    public int oxygen;
     public int oxygenLoss = 1; 
     public float oxygenLossInterval = 2f; 
 
+    
+    [SerializeField] private float moveSpeed = 55f;
+    [SerializeField] private float turningMoveSpeed = 15f;
+    [SerializeField] private Rotation rotationReference;
+    private Rigidbody2D rb;
+    private float moveHorizontal = 0f;
+    private float moveVertical = 0f;
+    
     private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         health = maxHealth;
         oxygen = maxOxygen;
         StartCoroutine(OxygenLossRoutine()); 
     }
+    void Update()
+    {
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
 
+        if (rotationReference.canMove)
+        {
+            if (moveHorizontal != 0 || moveVertical != 0)
+            {
+                rb.linearVelocity = new Vector2(moveHorizontal, moveVertical).normalized * moveSpeed;
+            }
+            else
+            {
+                rb.linearVelocity = Vector2.zero;
+            }
+        }
+        else
+        {
+            if (moveHorizontal != 0 || moveVertical != 0)
+            {
+                rb.linearVelocity = new Vector2(moveHorizontal, moveVertical).normalized * moveSpeed *
+                                    ((180 - Mathf.Abs(rotationReference.angleDiff))/180);
+            }
+            else
+            {
+                rb.linearVelocity = Vector2.zero;
+            }
+        }
+    }
+    
+    
     private IEnumerator OxygenLossRoutine()
     {
         while (oxygen > 0)
