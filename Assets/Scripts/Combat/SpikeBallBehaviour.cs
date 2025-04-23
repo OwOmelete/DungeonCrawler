7abcc51377d730990a21b25db54888f02e3c6216
OwOmelete <nathan.levylequart@gmail.com>
2025-04-23 19:01:12 +0200

    brotulo quasi fini

--- Assets/Scripts/Combat/SpikeBallBehaviour.cs
+++ Assets/Scripts/Combat/SpikeBallBehaviour.cs
@@ -1,8 +1,407 @@
+using System;
+using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class SpikeBallBehaviour : AbstractIA
 {
+    [SerializeField] SpikeData _spikeData;
+    private List<SpikeInstance> spikeList = new List<SpikeInstance>();
+
     public override void ManageTurn(FishDataInstance entity)
     {
+        if (entity.FirstCycle)
+        {
+            entity.FirstCycle = false;
+            entity.HasAttacked = true;
+            entity.entityChild = entity.prefab.transform.GetChild(0);
+        }
+
+        Debug.Log(entity.direction);
+        foreach (SpikeInstance spike in spikeList)
+        {
+            CombatManager.Instance.grid[spike.positionY, spike.positionX] = null;
+            if (spike.positionY + spike.dirY < 0 ||
+                spike.positionY + spike.dirY > CombatManager.Instance.grid.GetLength(0) - 1 ||
+                spike.positionX + spike.dirX < 0 ||
+                spike.positionX + spike.dirX > CombatManager.Instance.grid.GetLength(1) - 1)
+            {
+                spikeList.Remove(spike);
+                Destroy(spike.prefab);
+            }
+            else
+            {
+                EntityInstance newPos =
+                    CombatManager.Instance.grid[spike.positionY + spike.dirY, spike.positionX + spike.dirX];
+                if (newPos != null && newPos is not SpikeInstance)
+                {
+                    spikeList.Remove(spike);
+                    CombatManager.Instance.Damage(newPos, spike.hp);
+                    Destroy(spike.prefab);
+                }
+                else
+                {
+                    CombatManager.Instance.grid[spike.positionY + spike.dirY, spike.positionX + spike.dirX] = spike;
+                    spike.prefab.transform.DOMove(new Vector3
+                        (spike.positionX + spike.dirX, spike.positionY + spike.dirY, 0), 0.5f).SetEase(Ease.InOutCubic);
+                    spike.positionX += spike.dirX;
+                    spike.positionY += spike.dirY;
+                }
+            }
+        }
+
+        if (entity.PreparingAttack)
+        {
+            switch (entity.direction)
+            {
+                case EntityInstance.dir.up:
+                    if (CombatManager.Instance.player.positionX > entity.positionX)
+                    {
+                        TurnLeft(entity);
+                    }
+                    else
+                    {
+                        TurnRight(entity);
+                    }
+
+                    break;
+                case EntityInstance.dir.down:
+                    if (CombatManager.Instance.player.positionX > entity.positionX)
+                    {
+                        TurnRight(entity);
+                    }
+                    else
+                    {
+                        TurnLeft(entity);
+                    }
+
+                    break;
+                case EntityInstance.dir.left:
+                    if (CombatManager.Instance.player.positionY > entity.positionY)
+                    {
+                        TurnLeft(entity);
+                    }
+                    else
+                    {
+                        TurnRight(entity);
+                    }
+
+                    break;
+                case EntityInstance.dir.right:
+                    if (CombatManager.Instance.player.positionY > entity.positionY)
+                    {
+                        TurnRight(entity);
+                    }
+                    else
+                    {
+                        TurnLeft(entity);
+                    }
+
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            entity.PreparingAttack = false;
+        }
+        else
+        {
+            if (entity.HasAttacked)
+            {
+                entity.HasAttacked = false;
+                switch (entity.direction)
+                {
+                    case EntityInstance.dir.up:
+                        ShootSpike(entity, 0, -1);
+                        break;
+                    case EntityInstance.dir.down:
+                        ShootSpike(entity, 0, 1);
+                        break;
+                    case EntityInstance.dir.left:
+                        ShootSpike(entity, 1, 0);
+                        break;
+                    case EntityInstance.dir.right:
+                        ShootSpike(entity, -1, 0);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            else
+            {
+                entity.HasAttacked = true;
+            }
+
+            switch (entity.direction)
+            {
+                case EntityInstance.dir.up:
+                    if (CombatManager.Instance.player.positionY > entity.positionY &&
+                        SecondaryPlayerCoordsY() > entity.positionY)
+                    {
+                        FullPlayerDetected(entity);
+                    }
+                    else
+                    {
+                        if (CombatManager.Instance.player.positionY > entity.positionY ||
+                            SecondaryPlayerCoordsY() > entity.positionY)
+                        {
+                            HalfPlayerDetected(entity);
+                        }
+                    }
+
+                    break;
+                case EntityInstance.dir.down:
+                    if (CombatManager.Instance.player.positionY < entity.positionY &&
+                        SecondaryPlayerCoordsY() < entity.positionY)
+                    {
+                        FullPlayerDetected(entity);
+                    }
+                    else
+                    {
+                        if (CombatManager.Instance.player.positionY < entity.positionY ||
+                            SecondaryPlayerCoordsY() < entity.positionY)
+                        {
+                            HalfPlayerDetected(entity);
+                        }
+                    }
+
+                    break;
+                case EntityInstance.dir.left:
+                    if (CombatManager.Instance.player.positionX < entity.positionX &&
+                        SecondaryPlayerCoordsX() < entity.positionX)
+                    {
+                        FullPlayerDetected(entity);
+                    }
+                    else
+                    {
+                        if (CombatManager.Instance.player.positionX < entity.positionX ||
+                            SecondaryPlayerCoordsX() < entity.positionX)
+                        {
+                            HalfPlayerDetected(entity);
+                        }
+                    }
+
+                    break;
+                case EntityInstance.dir.right:
+                    if (CombatManager.Instance.player.positionX > entity.positionX &&
+                        SecondaryPlayerCoordsX() > entity.positionX)
+                    {
+                        FullPlayerDetected(entity);
+                    }
+                    else
+                    {
+                        if (CombatManager.Instance.player.positionX > entity.positionX ||
+                            SecondaryPlayerCoordsX() > entity.positionX)
+                        {
+                            HalfPlayerDetected(entity);
+                        }
+                    }
+
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+        }
+
+        void FullPlayerDetected(FishDataInstance entity)
+        {
+            Debug.Log("fullDetect");
+            entity.PreparingAttack = true;
+        }
+
+        void HalfPlayerDetected(FishDataInstance entity)
+        {
+            switch (entity.direction)
+            {
+                case EntityInstance.dir.up:
+                    if (CombatManager.Instance.player.positionX > entity.positionX)
+                    {
+                        ShootSpike(entity, 1, 0);
+                    }
+                    else
+                    {
+                        ShootSpike(entity, -1, 0);
+                    }
+
+                    break;
+                case EntityInstance.dir.down:
+                    if (CombatManager.Instance.player.positionX > entity.positionX)
+                    {
+                        ShootSpike(entity, 1, 0);
+                    }
+                    else
+                    {
+                        ShootSpike(entity, -1, 0);
+                    }
+
+                    break;
+                case EntityInstance.dir.left:
+                    if (CombatManager.Instance.player.positionY > entity.positionY)
+                    {
+                        ShootSpike(entity, 0, 1);
+                    }
+                    else
+                    {
+                        ShootSpike(entity, 0, -1);
+                    }
+
+                    break;
+                case EntityInstance.dir.right:
+                    if (CombatManager.Instance.player.positionY > entity.positionY)
+                    {
+                        ShootSpike(entity, 0, 1);
+                    }
+                    else
+                    {
+                        ShootSpike(entity, 0, -1);
+                    }
+
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        void ShootSpike(FishDataInstance entity, int dirX, int dirY)
+        {
+            if (CombatManager.Instance.grid[entity.positionX + dirX, entity.positionY + dirY] != null)
+            {
+                CombatManager.Instance.Damage(
+                    CombatManager.Instance.grid[entity.positionX + dirX, entity.positionY + dirY], _spikeData.hp);
+            }
+            else
+            {
+                SpikeInstance spike = new SpikeInstance(_spikeData);
+                spikeList.Add(spike);
+                spike.dirX = dirX;
+                spike.dirY = dirY;
+                spike.positionY = entity.positionY + dirY;
+                spike.positionX = entity.positionX + dirX;
+                CombatManager.Instance.grid[spike.positionY, spike.positionX] = spike;
+                int rotation;
+                if (dirX == 1)
+                {
+                    rotation = -90;
+                }
+                else if (dirX == -1)
+                {
+                    rotation = 90;
+                }
+                else if (dirY == 1)
+                {
+                    rotation = 0;
+                }
+                else
+                {
+                    rotation = 180;
+                }
+
+                spike.prefab = Instantiate(spike.prefab, new Vector3(entity.positionX + dirX,
+                    entity.positionY + dirY, 0), Quaternion.Euler(0, 0, rotation));
+                spike.entityChild = spike.prefab.transform.GetChild(0);
+            }
+        }
+
+        void TurnRight(FishDataInstance entity)
+        {
+            Debug.Log("turnRight");
+            switch (entity.direction)
+            {
+                case EntityInstance.dir.up:
+                    entity.direction = EntityInstance.dir.right;
+                    entity.weakPointList.Clear();
+                    entity.weakPointList.Add(entity.WeakPointsRight[0]);
+                    break;
+                case EntityInstance.dir.down:
+                    entity.direction = EntityInstance.dir.left;
+                    entity.weakPointList.Clear();
+                    entity.weakPointList.Add(entity.WeakPointsLeft[0]);
+                    break;
+                case EntityInstance.dir.left:
+                    entity.direction = EntityInstance.dir.up;
+                    entity.weakPointList.Clear();
+                    entity.weakPointList.Add(entity.WeakPointsUp[0]);
+                    break;
+                case EntityInstance.dir.right:
+                    entity.direction = EntityInstance.dir.down;
+                    entity.weakPointList.Clear();
+                    entity.weakPointList.Add(entity.WeakPointsDown[0]);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            entity.entityChild.DOLocalRotate(new Vector3(0, 0, entity.entityChild.localEulerAngles.z - 90), 0.5f)
+                .SetEase(Ease.InOutCubic);
+        }
+
+        void TurnLeft(FishDataInstance entity)
+        {
+            Debug.Log("turnLeft");
+
+            switch (entity.direction)
+            {
+                case EntityInstance.dir.up:
+                    entity.direction = EntityInstance.dir.left;
+                    entity.weakPointList.Clear();
+                    entity.weakPointList.Add(entity.WeakPointsLeft[0]);
+                    break;
+                case EntityInstance.dir.down:
+                    entity.direction = EntityInstance.dir.right;
+                    entity.weakPointList.Clear();
+                    entity.weakPointList.Add(entity.WeakPointsRight[0]);
+                    break;
+                case EntityInstance.dir.left:
+                    entity.direction = EntityInstance.dir.down;
+                    entity.weakPointList.Clear();
+                    entity.weakPointList.Add(entity.WeakPointsDown[0]);
+                    break;
+                case EntityInstance.dir.right:
+                    entity.direction = EntityInstance.dir.up;
+                    entity.weakPointList.Clear();
+                    entity.weakPointList.Add(entity.WeakPointsUp[0]);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            entity.entityChild.DOLocalRotate(new Vector3(0, 0, entity.entityChild.localEulerAngles.z + 90), 0.5f)
+                .SetEase(Ease.InOutCubic);
+        }
+
+        int SecondaryPlayerCoordsX()
+        {
+            switch (CombatManager.Instance.player.direction)
+            {
+                case EntityInstance.dir.up:
+                    return CombatManager.Instance.player.positionX;
+                case EntityInstance.dir.down:
+                    return CombatManager.Instance.player.positionX;
+                case EntityInstance.dir.left:
+                    return CombatManager.Instance.player.positionX - 1;
+                case EntityInstance.dir.right:
+                    return CombatManager.Instance.player.positionX + 1;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        int SecondaryPlayerCoordsY()
+        {
+            switch (CombatManager.Instance.player.direction)
+            {
+                case EntityInstance.dir.up:
+                    return CombatManager.Instance.player.positionY + 1;
+                case EntityInstance.dir.down:
+                    return CombatManager.Instance.player.positionY - 1;
+                case EntityInstance.dir.left:
+                    return CombatManager.Instance.player.positionY;
+                case EntityInstance.dir.right:
+                    return CombatManager.Instance.player.positionY;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
     }
 }
--- Assets/Scripts/Data/FishData.cs
+++ Assets/Scripts/Data/FishData.cs
@@ -4,10 +4,11 @@ using UnityEngine.UI;
 [CreateAssetMenu(fileName = "FishData", menuName = "Scriptable Objects/Entity/FishData")]
 public class FishData : Entity
 {
-    public string name;
     public AttackData[] attacks;
     public WeakPointData[] WeakPointsRight;
     public WeakPointData[] WeakPointsLeft;
+    public WeakPointData[] WeakPointsUp;
+    public WeakPointData[] WeakPointsDown;
     //Position weaknesses
     //effects weaknesses( ? )
     //spawn zone (corail/kayou/carcasse)
@@ -21,7 +22,6 @@ public class FishData : Entity
 
 public class FishDataInstance : EntityInstance
 {
-    public string name;
     public bool Flipped = false;
     public bool PreparingAttack = false;
     public bool HasAttacked = false;
@@ -30,11 +30,14 @@ public class FishDataInstance : EntityInstance
     public AttackData[] attacks;
     public WeakPointData[] WeakPointsRight;
     public WeakPointData[] WeakPointsLeft;
+    public WeakPointData[] WeakPointsUp;
+    public WeakPointData[] WeakPointsDown;
     public FishDataInstance(FishData data) : base(data)
     {
-        name = data.name;
         attacks = data.attacks;
         WeakPointsLeft = data.WeakPointsLeft;
         WeakPointsRight = data.WeakPointsLeft;
+        WeakPointsDown = data.WeakPointsDown;
+        WeakPointsUp = data.WeakPointsUp;
     }
 }
