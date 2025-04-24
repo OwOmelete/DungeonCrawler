02c9beb5621e715eb2e94b49bb97a0b1c8710e24
E Laffont <laffont.edu@gmail.com>
2025-04-24 11:17:00 +0200

    Rockfall / heal and volcano fix + GD friendly

--- Assets/Scripts/Edouard/FallingRocks.cs
+++ Assets/Scripts/Edouard/FallingRocks.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
-
+using UnityEngine.Serialization;
 
 public class FallingRocks : MonoBehaviour
 {
     public GameObject fallingRock;
-    //public float ySpawnCenter;
     public float activationChance = 50f; 
-    public int rockCount = 5; 
-    public float minX = -100f, maxX = 100f;
+    public int rockCount = 5;
+    [Header("Spawn horizontale")] public float minX = -100f;
+    public float maxX = 100f;
+    [Header("Spawn verticale")] public int minY = 10;
+    public int maxY = 10;
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -26,7 +28,7 @@ public class FallingRocks : MonoBehaviour
         for (int i = 0; i < rockCount; i++) 
         {
             float xSpawn = Random.Range(transform.position.x + minX, transform.position.x + maxX); 
-            float ySpawn = Random.Range(transform.position.y + 10, transform.position.y + 20);
+            float ySpawn = Random.Range(transform.position.y + minY, transform.position.y + (minY + maxY));
             Vector2 spawnPos = new Vector2(xSpawn, ySpawn);
         
             Instantiate(fallingRock, spawnPos, Quaternion.identity); 
