e65744e637675e29115b6f47be8e28e6167d8dec
OwOmelete <nathan.levylequart@gmail.com>
2025-06-02 15:21:23 +0200

    player start position combat

--- Assets/Scripts/Lucas/FishHitPlayer.cs
+++ Assets/Scripts/Lucas/FishHitPlayer.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class FishHitPlayer : MonoBehaviour
 {
+    [SerializeField] private int playerPosCombatX = 0;
+    [SerializeField] private int playerPosCombatY = 0;
+    
     [SerializeField] private StartCombat startCombatReference;
     [SerializeField] private GameObject fish;
     [SerializeField] private List<FishData> fishDatas;
@@ -12,6 +15,8 @@ public class FishHitPlayer : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            startCombatReference.playerPosCombatX = playerPosCombatX;
+            startCombatReference.playerPosCombatY = playerPosCombatY;
             startCombatReference.SwitchToCombat(fishDatas);
             fish.SetActive(false);
         }
