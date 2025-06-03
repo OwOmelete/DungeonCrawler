80162a94ef05bb038c28de4fb27dd09a22a860c5
Lucas <lucas.kominiarz@laposte.net>
2025-06-03 18:16:58 +0200

    radar light

--- Assets/Scripts/Lucas/LightManager.cs
+++ Assets/Scripts/Lucas/LightManager.cs
@@ -74,7 +74,7 @@ public class LightManager : MonoBehaviour
     public void AddLight(float value)   // recharge la lumière
     {
         player.light += value;
-        player.light = Mathf.Clamp(player.oxygen, minLight, maxLight);
+        player.light = Mathf.Clamp(player.light, minLight, maxLight);
         DOTween.To(() => playerLight.pointLightOuterRadius, x => 
             playerLight.pointLightOuterRadius = x, player.light, lerpDuration);
         DOTween.To(() => playerLight.pointLightInnerRadius, x => 
