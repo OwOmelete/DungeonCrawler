c7b246c7e9bd926e065fd712b9ecca2070591f06
E Laffont <laffont.edu@gmail.com>
2025-06-03 10:33:58 +0200

    normalement le menu fonctionne pour manette

--- Assets/Scripts/Edouard/MainMenuManager.cs
+++ Assets/Scripts/Edouard/MainMenuManager.cs
@@ -1,28 +1,42 @@
-using System;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class MainManuManager : MonoBehaviour
+public class MainMenuManager : MonoBehaviour
 {
-    
     public GameObject menuCanvas;
     public GameObject menuImage;
+    public Button firstSelectedButton;
+
     private void Start()
     {
         Time.timeScale = 0;
         menuCanvas.SetActive(true);
         menuImage.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(firstSelectedButton.gameObject);
     }
 
     public void StartGame()
     {
         menuCanvas.SetActive(false);
         menuImage.SetActive(false);
-       Time.timeScale = 1;
+        Time.timeScale = 1;
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void GoToMainMenu()
+    {
+        menuCanvas.SetActive(true);
+        menuImage.SetActive(true);
+        Time.timeScale = 0;
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(firstSelectedButton.gameObject);
     }
 }
