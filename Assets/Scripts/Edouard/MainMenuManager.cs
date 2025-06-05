80a36467cfedbe789439d359ed3e1965bfec3209
E Laffont <laffont.edu@gmail.com>
2025-06-05 11:39:51 +0200

    Retour au menu avec manette

--- Assets/Scripts/Edouard/MainMenuManager.cs
+++ Assets/Scripts/Edouard/MainMenuManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
@@ -8,14 +9,26 @@ public class MainMenuManager : MonoBehaviour
     public GameObject menuImage;
     public Button firstSelectedButton;
 
+    public Input input; 
+
     private void Start()
     {
+        //input.Enable(); 
+
         Time.timeScale = 0;
         menuCanvas.SetActive(true);
         menuImage.SetActive(true);
 
         EventSystem.current.SetSelectedGameObject(null);
         EventSystem.current.SetSelectedGameObject(firstSelectedButton.gameObject);
+    }
+
+    private void Update()
+    {
+        if (input.Equals(enabled)/*UI.GoBack.triggered*/) 
+        {
+            GoToMainMenu();
+        }
     }
 
     public void StartGame()
